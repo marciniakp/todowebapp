74a73cc2bd230592fab62680543d2bda90932a1f
PrzemysławMarciniak <m.przemek94@gmail.com>
2018-10-02 16:23:04 +0200

    commit with jar

--- src/main/java/com/betapp/model/Customer.java
+++ src/main/java/com/betapp/model/Customer.java
@@ -1,7 +1,6 @@
 package com.betapp.model;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
